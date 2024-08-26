83241d1d3d16c7c28a3cf84fca492f3c5cddc299
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-08-26 13:18:58 +0800

    Initial logo and nav setup

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,27 @@
+'use client';
 import ThemeToggler from "./ThemeToggler"
+import { useState, useEffect } from "react";
+import Logo from "./Logo";
+import Nav from './Nav';
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
     <header>
-        <ThemeToggler />
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <Logo />
+          <div>
+            {/* Desktop Nav */}
+            <Nav />
+            <ThemeToggler />
+            {/* Mobile Nav */}
+            <div>
+              <MobileNav />
+            </div>
+          </div>
+        </div>
+      </div>
     </header>
   )
 }
