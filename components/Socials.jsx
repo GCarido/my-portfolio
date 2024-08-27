24bd1e65f4588fddec7b3803dadc15f7774e8612
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-08-27 16:18:57 +0800

    Add socials component in header

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+'use client';
 
-const Socials = () => {
+import { RiLinkedinFill, RiGithubFill, RiFacebookBoxFill } from 'react-icons/ri'
+
+import Link from 'next/link';
+
+const icons = [
+  {
+    path: 'https://www.linkedin.com/in/gerald-carido/',
+    name: <RiLinkedinFill />
+  },
+  {
+    path: 'https://github.com/GCarido',
+    name: <RiGithubFill />
+  },
+  {
+    path: 'https://www.facebook.com/GeraldCarido',
+    name: <RiFacebookBoxFill />
+  }
+]
+
+const Socials = ({ containerStyles, iconStyles }) => {
   return (
-    <div>Socials</div>
-  )
-}
+    <div className={`${containerStyles}`}>
+      {icons.map((icon, index) => {
+        return (
+        <Link 
+        href={icon.path} 
+        key={index}
+        target="_blank"
+        >
+          <div className={iconStyles}>{icon.name}</div>
+        </Link>
+        );
+
+      })}
+    </div>
+  );
+};
 
 export default Socials
