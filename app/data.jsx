88d61163c6a4ee7925933cd658f179b373073f5a
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-08-29 20:25:44 +0800

    Separate project data into another file

---
+++ app/data.jsx
@@ -0,0 +1,60 @@
+const projectData = [
+    {
+        image: '/work/3.png',
+        category: 'react js',
+        name: 'Sample Website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel ex dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+        link: '/',
+        github: '/'
+    },
+    {
+        image: '/work/4.png',
+        category: 'c#',
+        name: 'Sample Website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel ex dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+        link: '/',
+        github: '/'
+    },
+    {
+        image: '/work/2.png',
+        category: 'full stack',
+        name: 'Sample Website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel ex dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+        link: '/',
+        github: '/'
+    },
+    {
+        image: '/work/1.png',
+        category: 'c#',
+        name: 'Sample Website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel ex dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+        link: '/',
+        github: '/'
+    },
+    {
+        image: '/work/3.png',
+        category: 'c#',
+        name: 'Sample Website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel ex dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+        link: '/',
+        github: '/'
+    },
+    {
+        image: '/work/2.png',
+        category: 'react js',
+        name: 'Sample Website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel ex dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+        link: '/',
+        github: '/'
+    },
+    {
+        image: '/work/2.png',
+        category: 'react js',
+        name: 'Sample Website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel ex dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+        link: '/',
+        github: '/'
+    },
+];
+
+export default projectData;
