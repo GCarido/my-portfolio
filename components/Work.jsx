f99e6178b2f0d1df19bae5c5591448d1316a3b21
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-08-29 20:28:45 +0800

    Add work content

---
+++ components/Work.jsx
@@ -0,0 +1,50 @@
+'use client';
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import { Pagination } from 'swiper/modules';
+
+import ProjectCard from '@/components/ProjectCard';
+
+import projectData from '@/app/data';
+
+const Work = () => {
+    return (
+        <section className="relative mb-12 xl:mb-48">
+            <div className="container mx-auto">
+                {/* text */}
+                <div className="max-w-[400px] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start">
+                    <h2 className="section-title mb-4">Latest Projects</h2>
+                    <p className="subtitle mb-8">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste dignissimos sequi amet facere, minima cum delectus illum reiciendis, libero quibusdam perferendis cumque laudantium, fuga est molestias numquam rem cupiditate architecto.</p>
+                    <Link href="/projects"><Button>All projects</Button></Link>
+                </div>
+                {/* slider */}
+                <div className="xl:max-w-[1000px] xl:absolute right-0 top-0">
+                    <Swiper className="h-[500px] " slidesPerView={1} breakpoints={{
+                        640: {
+                            slidesPerView: 2
+                        }
+                    }}
+                        spaceBetween={30}
+                        modules={[Pagination]}
+                        pagination={{ clickable: true }}
+                    >
+                        {/* show 4 projects  */}
+                        {projectData.slice(0, 4).map((project, index) => {
+                            return <SwiperSlide key={index}>
+                                <ProjectCard project={project} />
+                            </SwiperSlide>
+                        })}
+                    </Swiper>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Work
