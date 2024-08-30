19e6d0aaa0fc70d303391f229a2dfce2651e8226
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-08-30 17:01:25 +0800

    Justify text style

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -33,7 +33,7 @@ const ProjectCard = ({ project }) => {
             <div className="h-full px-8 py-6">
                 <Badge className="uppercase text-sm font-medium mb-2 absolute top-4 left-5">{project.category}</Badge>
                 <h4 className="h4 mb-1">{project.name}</h4>
-                <p className="text-muted-foreground text-lg">{project.description}</p>
+                <p className="text-muted-foreground text-lg text-justify">{project.description}</p>
             </div>
         </Card>
     )
