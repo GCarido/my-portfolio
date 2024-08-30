5174c7fc26d69f57f06e3483dcf26a76b0d9d425
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-08-30 17:11:58 +0800

    Edit placeholder text

--- app/data.jsx
+++ app/data.jsx
@@ -1,57 +1,50 @@
 const projectData = [
     {
-        image: '/work/3.png',
+        image: '/work/portfolio-1.png',
         category: 'react js',
-        name: 'Sample Website',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel ex dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+        name: 'Web Portfolio',
+        description: 'Developed a portfolio website using React.js, Next.js, and ShadCN to showcase projects, optimize performance with static site generation, and enhance UI/UX with modern design components.',
         link: '/',
-        github: '/'
+        github: 'https://github.com/GCarido/my-portfolio'
     },
     {
-        image: '/work/4.png',
+        image: '/work/borrowing-system1.png',
         category: 'c#',
-        name: 'Sample Website',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel ex dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
-        link: '/',
-        github: '/'
+        name: 'Borrowing System',
+        description: 'Developed a web application to track borrowed equipment, check availability for students, reduce time and costs associated with manual forms, and enhance data security.',
+        link: '/notice',
+        github: 'https://github.com/GCarido/Borrowing-System-V3'
     },
     {
-        image: '/work/2.png',
+        image: '/work/hr-flow-1.png',
         category: 'full stack',
-        name: 'Sample Website',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel ex dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
-        link: '/',
-        github: '/'
+        name: 'HR Flow',
+        description: 'A Human Resource Information System (HRIS) that enhances operational efficiency and providing a seamless experience from employee onboarding to payroll processing in the corporate landscape.',
+        link: '/notice',
+        github: 'https://github.com/GCarido/HRFlow'
     },
+
     {
-        image: '/work/1.png',
+        image: '/work/ats1.png',
         category: 'c#',
-        name: 'Sample Website',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel ex dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
-        link: '/',
-        github: '/'
+        name: 'Organization Management System (ATS)',
+        description: 'Developed a desktop application to track employee records, manage attendance digitally, eliminate manual time-tracking methods, and enhance data security for organizations.',
+        link: '/notice',
+        github: 'https://github.com/GCarido/OMS-Attendance'
     },
     {
-        image: '/work/3.png',
+        image: '/work/grade-1.png',
         category: 'c#',
-        name: 'Sample Website',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel ex dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+        name: 'Grade Calculator',
+        description: 'Helps college students calculate their current grades and determine the scores needed in activities to achieve their desired course grade for both midterms and finals, allowing input of numerical scores, subject choices, and student details.',
         link: '/',
         github: '/'
     },
     {
-        image: '/work/2.png',
-        category: 'react js',
-        name: 'Sample Website',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel ex dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
-        link: '/',
-        github: '/'
-    },
-    {
-        image: '/work/2.png',
-        category: 'react js',
-        name: 'Sample Website',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel ex dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+        image: '/work/budget-1.png',
+        category: 'c#',
+        name: 'Easey Budget',
+        description: 'Easey Budget is an offline desktop application for individuals and small businesses to manage their finances, track expenses, income, and transactions without needing internet connectivity which also includes the use of a google map feature.',
         link: '/',
         github: '/'
     },
