59511b623ca83e93a3a7fda1324185ff15655626
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-09-02 00:22:20 +0800

    Add route logic for email sending

---
+++ app/api/email/route.ts
@@ -0,0 +1,25 @@
+import { EmailTemplate } from '../../../components/EmailTemplate';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(request: Request) {
+  try {
+    const { name, email, message } = await request.json();
+
+    const { data, error } = await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['geraldcastcarido@gmail.com'],
+      subject: 'Portfolio Email Received',
+      react: EmailTemplate({ firstName: name, email, message }),
+    });
+
+    if (error) {
+      return new Response(JSON.stringify({ error }), { status: 500 });
+    }
+
+    return new Response(JSON.stringify(data));
+  } catch (error) {
+    return new Response(JSON.stringify({ error }), { status: 500 });
+  }
+}
